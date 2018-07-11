116d3b2607767958da437993fe46ac1c2c35df0b
Ryanden <lockstom@gmail.com>
2018-07-11 21:53:18 +0900

    add production

---
+++ app/config/settings/production.py
@@ -0,0 +1,36 @@
+from .base import *
+# secrets = json.loads(open(os.path.join(SECRETS_DIR, 'dev.json')).read())
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'localhost',
+]
+
+# django storages settings
+INSTALLED_APPS += [
+    'storages',
+]
+
+DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
+STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = secrets['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = secrets['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = secrets['AWS_S3_SIGNATURE_VERSION']
+
+# Static
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+
+# WSGI
+WSGI_APPLICATION = 'config.wsgi.production.application'
+
+# DB
+DATABASES = secrets['DATABASES']
